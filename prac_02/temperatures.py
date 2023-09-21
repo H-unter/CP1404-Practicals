f1cb022eb8c9ffbb90e83664c1862efdbe1b24ce
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-21 12:02:48 +1000

    Refactor temperatures.py to include function, and alter variable names in main to be more clear

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,24 +1,43 @@
 """
-CP1404/CP5632 - Practical
-Pseudocode for temperature conversion
+CP1404 Practical 02 Hunter Kruger-Ilingworth
+Temperature conversion script
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} F")
-    else:
-        print("Invalid option")
+
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius_in = float(input("Celsius: "))
+            fahrenheit_out = convert_celsius_to_fahrenheit(celsius_in)
+            print(f"Result: {fahrenheit_out:.2f} F")
+        elif choice == "F":
+            fahrenheit_in = float(input("Fahrenheit: "))
+            celsius_out = convert_fahrenheit_to_celsius(fahrenheit_in)
+            print(f"Result: {celsius_out:.2f} F")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """converts Fahrenheit to Celsius"""
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    """converts Celsius to Fahrenheit"""
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+
+main()
