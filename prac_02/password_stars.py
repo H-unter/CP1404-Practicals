65b2f6bb855534d2e8327461b0f6fc0e35480c1c
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-21 11:25:53 +1000

    Add password check program, password_stars.py

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,25 @@
 """Hunter Kruger-Ilingworth write a program that asks the user for a password, with error-checking to repeat if the
 password doesn't meet a minimum length set by a variable. The program should then print asterisks as long as the
 word. Example: if the user enters Pythonista (10 characters), the program should print **********."""
-minimum_length = 3
-password = input("Enter password: ")
-while len(password) < minimum_length:
-    print(f"Password must be at least {minimum_length} characters long.")
-    password = input("Renter password: ")
 
-print(len(password) * "*")
+
+def main():
+    minimum_length = 3
+    password = get_password(minimum_length)
+
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print(len(password) * "*")
+
+
+def get_password(minimum_length):
+    password = input("Enter password: ")
+    while len(password) < minimum_length:
+        print(f"Password must be at least {minimum_length} characters long.")
+        password = input("Renter password: ")
+    return password
+
+
+main()
