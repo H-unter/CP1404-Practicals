13de7f9f061e3d30a23a300b1ca7f447a63e2e2e
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-21 12:10:35 +1000

    Refactor score.py to include functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,22 @@ CP1404 Practical 01 Hunter Kruger-Ilingworth
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+
+def main():
     score = float(input("Enter score: "))
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    print(determine_performance(score))
+
+
+def determine_performance(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
