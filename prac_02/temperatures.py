62f6573ac024b4592bd5129a25b5d55fbd4984ff
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-21 11:48:12 +1000

    Add score.py and temperatures.py from previous prac

---
+++ prac_02/temperatures.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = celsius * 9.0 / 5 + 32
+        print(f"Result: {fahrenheit:.2f} F")
+    elif choice == "F":
+        fahrenheit = float(input("Fahrenheit: "))
+        celsius = 5 / 9 * (fahrenheit - 32)
+        print(f"Result: {celsius:.2f} F")
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
