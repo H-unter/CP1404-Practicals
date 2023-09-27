fae7eeaf9286275b307476062a4326f105c2b5d9
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-28 07:20:31 +1000

    Remove console code; add questions and answers to docstring

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,5 +1,25 @@
 import random
 
-print(random.randint(5, 20))  # line 1
-print(random.randrange(3, 10, 2))  # line 2
-print(random.uniform(2.5, 5.5))  # line 3
+"""What did you see on line 1?
+>> 8
+What was the smallest number you could have seen, 
+>> 5
+what was the largest?
+>> 20
+
+What did you see on line 2?
+>> 9
+What was the smallest number you could have seen, 
+>> 3
+what was the largest?
+>> 9
+Could line 2 have produced a 4?
+>> no
+
+What did you see on line 3?
+>> 5.039970525500413
+What was the smallest number you could have seen, 
+>> 2.5
+what was the largest?
+>> 5.5
+"""
