f1a7800e1c8dd06d70a7114b19a6f1899477108a
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-28 08:10:14 +1000

    Answer questions in docstring and eliminate the potential for ZeroDivisionError

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,17 +2,19 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+>>  when either the numerator or denominator are not numbers
 2. When will a ZeroDivisionError occur?
-3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+>>  when the denominator is 0, and the numerator is a number
 """
-
+denominator = 0
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print(f"Denominator cannot be zero!")
+        denominator = int(input("Re-enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
 print("Finished.")
