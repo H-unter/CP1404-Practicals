e67ba033939ee1951f98e13e7cd85c7d7b7f85d5
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-28 07:09:22 +1000

    Add incrementing numbers output

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -34,9 +34,5 @@ for i, number in enumerate(numbers, 1):
 
 print(f"{year} {name} for about ${cost:,.0f}!")
 
-# TODO: Using a for loop with the range function and string formatting,
-# produce the following right-aligned output (DO NOT use a list):
-#   0
-#  50
-# 100
-# 150
+for i in range(0, 151, 50):
+    print(f"{i:4}")
