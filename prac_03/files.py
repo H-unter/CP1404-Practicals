efd497f3fa4717cb30811e79b16f87e22bba590e
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-28 08:59:43 +1000

    Read from name.txt and print result

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,15 +1,22 @@
-"""Write code that asks the user for their name, then opens a file called "name.txt" and writes that name to it. Remember to close your file.
+"""Write code that asks the user for their name, then opens a file called "name.txt" and writes that name to it.
+Remember to close your file.
 
-(In the same file, but as if it were a separate program) Write code that opens "name.txt" and reads the name (as above) then prints,
-"Your name is Bob" (or whatever the name is in the file).
+(In the same file, but as if it were a separate program) Write code that opens "name.txt" and reads the name (as
+above) then prints, "Your name is Bob" (or whatever the name is in the file).
 
-Create a text file called numbers.txt and save it in your prac directory. Put the following three numbers on separate lines in the file and save it:
-17
-42
-400
-Write code that opens "numbers.txt", reads only the first two numbers and adds them together then prints the result, which should be... 59.
+Create a text file called numbers.txt and save it in your prac directory. Put the following three numbers on separate
+lines in the file and save it: 17 42 400 Write code that opens "numbers.txt", reads only the first two numbers and
+adds them together then prints the result, which should be... 59.
 
-Now write a fourth block of code that prints the total for all lines in numbers.txt or a file with any number of numbers."""
+Now write a fourth block of code that prints the total for all lines in numbers.txt or a file with any number of
+numbers."""
+
+name_file = "name.txt"
+
 name = input("What is your name? ")
-with open("name.txt", "w") as out_file:
-    print(f"Name: {name}", file=out_file)
+with open(name_file, "w") as file:
+    print(f"{name}", file=file)
+
+with open(name_file, "r") as file:
+    text = file.read()
+    print(f"Your name is {text}")
