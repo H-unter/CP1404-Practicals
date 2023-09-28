e85f01b2d17d3c52dea729b328a5a5ed02a96227
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-28 11:46:41 +1000

    Shorten exceptions_to_complete.py by removing redundant else statement

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -7,8 +7,7 @@ is_finished = False
 while not is_finished:
     try:
         result = int(input("Enter a valid integer: "))
+        is_finished = True
     except ValueError:
         print("Please enter a valid integer.")
-    else:
-        is_finished = True
 print("Valid result is:", result)
