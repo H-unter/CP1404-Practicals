05588e310fb98cd0be7cb201d6678676ca024af7
H-unter <99230734+H-unter@users.noreply.github.com>
2023-09-28 10:05:22 +1000

    Finalise functionality of is_valid_password()

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -47,9 +47,10 @@ def is_valid_password(password):
             count_special += 1
 
     # TODO: if any of the 'normal' counts are zero, return False
-
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
+    if (count_lower == 0) or (count_upper == 0) or (count_digit == 0):
+        return False
+    if SPECIAL_CHARS_REQUIRED and (count_special == 0):
+        return False
 
     # if we get here (without returning False), then the password must be valid
     return True
