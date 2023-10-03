3e5e22e57f048c5ba4783cc2b2cc49ea9d9db449
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-04 08:26:07 +1000

    Refactor main to extract report printing into a separate function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,15 +7,19 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
