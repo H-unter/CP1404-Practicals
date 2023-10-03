d7ac0bf377bbefe35ee004883865446ddd19bf5e
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-04 07:45:56 +1000

    Add lists_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,11 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+print(numbers[0])  # Output: 3
+print(numbers[-1])  # Output: 5
+print(numbers[3])  # Output: 1
+print(numbers[:-1])  # Output: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+print(numbers[3:4])  # Output: [1]
+print(5 in numbers)  # Output: True
+print(7 in numbers)  # Output: False
+print("3" in numbers)  # Output: False
+print(numbers + [6, 5, 3])  # Output: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 6, 5, 3]
