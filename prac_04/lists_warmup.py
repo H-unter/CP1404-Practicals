9037affc84b119d66b07432ed4fccc30a90c0b31
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-04 07:57:36 +1000

    Complete lists_warmup.py

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,5 +1,4 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
-numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 print(numbers[0])  # Output: 3
 print(numbers[-1])  # Output: 5
 print(numbers[3])  # Output: 1
@@ -9,3 +8,9 @@ print(5 in numbers)  # Output: True
 print(7 in numbers)  # Output: False
 print("3" in numbers)  # Output: False
 print(numbers + [6, 5, 3])  # Output: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 6, 5, 3]
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(len(numbers))
+print([numbers[i] for i in range(len(numbers)) if i > 1])
+print(9 in numbers)
