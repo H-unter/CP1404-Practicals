8f33ba557056c6bd4bfc770878dbc82d335404f0
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-05 11:38:41 +1000

    Add password checking functionality to list_exercises.py

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,3 +12,12 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username_input = input("Username: ")
+if username_input in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
