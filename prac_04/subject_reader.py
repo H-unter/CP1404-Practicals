76ee157e821a6a7ffe7479cdca16850554dc052b
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-05 11:16:03 +1000

    Remove unnecessary print statements

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -15,13 +15,10 @@ def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
+        print(f"{parts}")  # See if that worked
         print("----------")
     input_file.close()
 
