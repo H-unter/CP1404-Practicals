dcc17765412129c7019ce6f0306e3d4e5a6b4f73
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-05 12:00:51 +1000

    Add quick_picks.py

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,8 @@
+"""Write a program that asks the user how many "quick picks" they wish to generate. The program then generates that
+many lines of output. Each line consists of 6 random numbers between 1 and 45. These values should be stored as
+CONSTANTS.
+
+Each line (quick pick) should not contain any repeated number.
+Each line of numbers should be displayed in sorted (ascending) order.
+Study the formatting below so that numbers align neatly.
+"""
