df16d0dbd1369eb45a31a3393b00cb434f4d6f56
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-05 11:24:58 +1000

    Add list_exercises.py

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,3 @@
+"""Write a program that prompts the user for 5 numbers and then stores each of these in a list called numbers.
+The program should then output information about these numbers, as in the output below.
+You can use the functions min, max, sum and len, and you can use the append method to add a number to a list."""
