a79cdf5916d61f19f0c7ab22f2024a63e04a375b
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-05 11:43:59 +1000

    Create a list of all the full_names in lowercase format

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -29,8 +29,7 @@ print(a_names)
 # 'Ada Alan Angel Bob Jimi'
 print(" ".join(sorted(names)))
 
-# TODO: list comprehension to create a list of all the full_names in lowercase format
-# lowercase_full_names =
+lowercase_full_names = [name.lower() for name in full_names]
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: list comprehension to create a list of integers from the above list of strings
