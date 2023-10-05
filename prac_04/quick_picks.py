5a6d04af704e742f67e2ad97ce69a863eacba2b4
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-05 12:33:00 +1000

    Add get_random_list()

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,6 +7,8 @@ Each line of numbers should be displayed in sorted (ascending) order.
 Study the formatting below so that numbers align neatly.
 """
 
+import random
+
 
 def main():
     quick_picks_number = int(input("How many quick picks? "))
@@ -16,7 +18,18 @@ def main():
     row_length = quick_picks_number
 
     for row in range(row_length):
-        print(column_length * " test ")
+        number_list = get_random_list(column_length)
+        print(', '.join([str(number) for number in number_list]))
+
+
+def get_random_list(column_length):
+    number_list = []
+    for column in range(column_length):
+        new_number = random.randint(1, 45)
+        while new_number in number_list:  # ensure no repeated numbers
+            new_number = random.randint(1, 45)
+        number_list.append(new_number)
+    return sorted(number_list)
 
 
 main()
