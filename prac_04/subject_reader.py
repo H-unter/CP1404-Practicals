79fb25f4d38dc20d96a13ba684a7e95a2360f5f7
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-05 11:23:16 +1000

    Correct get_data() to return nested list and display subject details in main()

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,19 +8,21 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print([f"{data[i][0]} is taught by {data[i][1]} and has {data[i][2]} students" for i in range(len(data))])
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(f"{parts}")  # See if that worked
-        print("----------")
+        data.append(parts)
     input_file.close()
+    return data
 
 
 main()
