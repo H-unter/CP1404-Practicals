df3d39b4e4b627b0e426627982466cd43129f309
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-07 10:06:55 +1000

    Add constants as an alternative to hard-coded limits

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -12,12 +12,14 @@ Study the formatting below so that numbers align neatly.
 
 import random
 
+QUICK_PICK_LENGTH = 6
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+
 
 def main():
-    # TODO: Add constants
     quick_picks_number = int(input("How many quick picks? "))
-
-    column_length = 6
+    column_length = QUICK_PICK_LENGTH
     row_length = quick_picks_number
 
     for row in range(row_length):
@@ -28,9 +30,9 @@ def main():
 def get_random_list(column_length):
     numbers = []
     for column in range(column_length):
-        new_number = random.randint(1, 45)
+        new_number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
         while new_number in numbers:  # ensure no repeated numbers
-            new_number = random.randint(1, 45)
+            new_number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
         numbers.append(new_number)
     return sorted(numbers)
 
