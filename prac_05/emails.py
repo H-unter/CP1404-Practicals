8f810f4910cf5e198c9bec27237ae5845e6c3097
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-11 08:46:26 +1000

    Implement rudimentary functionality of email prompting and processing

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,7 +1,23 @@
 """Start 8:31"""
 
-email = input("Enter input: ").split("@")
-print(email)
-while email != "":
-    email = input("Enter input: ").split("@")
-    print(email)
+
+def main():
+    email = get_email_and_name()
+    while email != "":
+        email = get_email_and_name()
+
+
+def get_email_and_name():
+    email = input("Enter input: ")
+    while len(email.split("@")) != 2:
+        email = input("INVALID! Re-enter input: ")
+    name = email.split("@")[0]
+    prompt = input(f"is {name} your name?").lower()
+    while prompt not in ['y', 'n']:
+        prompt = input(f"Come again? is {name} your name?").lower()
+    if prompt == 'n':
+        print("Drats, enter your actual name:")
+    return email
+
+
+main()
