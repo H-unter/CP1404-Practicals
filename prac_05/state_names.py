eb79fdd54d7e6f623502dd1483b5a733fcfb7a0d
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-10 15:46:52 +1000

    Copy state_names.py code

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,8 +1,18 @@
-file_name = "countries.csv"
-countries = {}
-with open(file_name, "r") as in_file:
-    for line in in_file:
-        if line != "Country (or territory),Capital,Population,% of country,Source":
-            print(line)
-            new_line = line.strip().split(',')
-            countries[new_line[0]] = new_line[1:]
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(CODE_TO_NAME)
+
+state_code = input("Enter short state: ")
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+    state_code = input("Enter short state: ")
