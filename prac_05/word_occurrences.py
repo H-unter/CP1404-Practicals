1d87363b7a23867e7d14e7ffe830853d7bf9e33b
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-11 08:23:58 +1000

    Print word frequency to terminal

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,16 +1,15 @@
 """
 Word Occurrences
 Estimate: 35 minutes
-Actual:    minutes
-start 8:18am
+Actual: 5 minutes
 """
 
 sentence = input("Text: ").strip().split(" ")  # error check
 word_to_frequency = {}
 for word in sentence:
     if word in list(word_to_frequency.keys()):
-        print("DETECTED!")
         word_to_frequency[word] += 1
     else:
-        print("NEW WORD!")
         word_to_frequency[word] = 1
+
+print(word_to_frequency)
