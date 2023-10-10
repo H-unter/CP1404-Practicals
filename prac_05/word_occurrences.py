14c3be9cfb0e40f70213972c0a1588f7e0a1884b
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-11 08:29:38 +1000

    Cleanup formatting of output

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,14 +2,17 @@
 Word Occurrences
 Estimate: 35 minutes
 Actual: 5 minutes
+8:18 start
 """
 
 sentence = input("Text: ").strip().split(" ")  # error check
 word_to_frequency = {}
+largest_word_length = max(len(word) for word in sentence)
+
 for word in sentence:
     if word in list(word_to_frequency.keys()):
         word_to_frequency[word] += 1
     else:
         word_to_frequency[word] = 1
-
-print(word_to_frequency)
+for word, frequency in word_to_frequency.items():
+    print(f"{word:{largest_word_length}} : {frequency}")
