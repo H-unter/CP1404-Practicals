13df505a283999b1b99acded19ed6d0aa11e40f5
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-10 16:10:22 +1000

    Add hex_colours.py

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,14 @@
+COLOUR_TO_HEX = {
+    'Absolute Zero': '#0048ba',
+    'Acid Green': '#b0bf1a',
+    'AliceBlue': '#f0f8ff',
+    'Alizarin crimson': '#e32636',
+    'Amaranth': '#e52b50',
+    'Amber': '#ffbf00',
+    'Amethyst': '#9966cc',
+    'AntiqueWhite': '#faebd7',
+    'AntiqueWhite1': '#ffefdb',
+    'AntiqueWhite2': '#eedfcc'
+}
+
+print(COLOUR_TO_HEX)
