669c5509a0fcbefff9a8757a1b81530edc811bd5
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-12 13:00:04 +1000

    Refactor to include functions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -19,25 +19,43 @@ AUS, CRO, ESP, FRG, GBR, GER, NED, SRB, SUI, SWE, TCH, USA
 """
 import csv
 
-name_to_times_won = {}
-victorious_countries = []
 
-file_name = 'wimbledon.csv'
-with open(file_name, "r", encoding="utf-8-sig") as in_file:
-    reader = csv.reader(in_file)
-    next(reader)  # remove header
-    for line in reader:
-        country = line[1]
-        name = line[2]
-        if country not in victorious_countries:
-            victorious_countries.append(country)
-        if name in list(name_to_times_won.keys()):
-            name_to_times_won[name] += 1
-        else:
-            name_to_times_won[name] = 1
-print(f"Wimbledon Champions:")
-for name, times_won in name_to_times_won.items():
-    print(f"{name} {times_won}")
-victorious_countries.sort()
-print(f"These {len(victorious_countries)} countries have won Wimbledon: ")
-print(', '.join([country for country in victorious_countries]))
+def main():
+    name_to_times_won = {}
+    victorious_countries = []
+
+    file_name = 'wimbledon.csv'
+    populate_data(file_name, name_to_times_won, victorious_countries)
+    print_champions_data(name_to_times_won)
+    print_countries_data(victorious_countries)
+
+
+def print_countries_data(victorious_countries):
+    victorious_countries.sort()
+    print(f"These {len(victorious_countries)} countries have won Wimbledon: ")
+    print(', '.join([country for country in victorious_countries]))
+
+
+def print_champions_data(name_to_times_won):
+    print(f"Wimbledon Champions:")
+    for name, times_won in name_to_times_won.items():
+        print(f"{name} {times_won}")
+
+
+def populate_data(file_name, name_to_times_won, victorious_countries):
+    """"Populate name_to_times_won and victorious_countries based on contents of wimbledon.csv"""
+    with open(file_name, "r", encoding="utf-8-sig") as in_file:
+        reader = csv.reader(in_file)
+        next(reader)  # remove header from file reading
+        for line in reader:
+            country = line[1]
+            name = line[2]
+            if country not in victorious_countries:
+                victorious_countries.append(country)
+            if name in list(name_to_times_won.keys()):
+                name_to_times_won[name] += 1
+            else:
+                name_to_times_won[name] = 1
+
+
+main()
