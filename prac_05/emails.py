d39589387e47368b7ed3b225d4f450b1969d54cb
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-12 11:29:39 +1000

    Append email and name to dictionary

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,23 +1,31 @@
-"""Start 8:31"""
+"""
+Start 8:31
+Working 8:46"""
 
 
 def main():
-    email = get_email_and_name()
-    while email != "":
-        email = get_email_and_name()
+    name_to_email = {}
+
+    while get_pair() != "":
+        pair = get_pair()
+        print(f"to be appended:{pair}")
+        name_to_email[pair[0]] = pair[1]
+        print(f"dictionary: {name_to_email}")
 
 
-def get_email_and_name():
-    email = input("Enter input: ")
-    while len(email.split("@")) != 2:
+def get_pair():
+    email = input("Email: ")
+    while len(email.split("@")) != 2:  # only 1 @ symbol is present in a valid email
         email = input("INVALID! Re-enter input: ")
     name = email.split("@")[0]
-    prompt = input(f"is {name} your name?").lower()
+    prompt = input(f"is your name {name}? (Y/N) ").lower()
     while prompt not in ['y', 'n']:
-        prompt = input(f"Come again? is {name} your name?").lower()
+        print("Invalid response.")
+        prompt = input(f"is your name {name}? (Y/N) ").lower()
     if prompt == 'n':
-        print("Drats, enter your actual name:")
-    return email
+        name = input("Name: ")
+
+    return [name, email]
 
 
 main()
