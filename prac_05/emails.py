a775241ec18e8a75d69a4f7e79fa5723a09a5a5b
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-12 11:36:07 +1000

    Adjust program to align closer to the prescribed specifications

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,24 +5,23 @@ Working 8:46"""
 
 def main():
     name_to_email = {}
-
+    pair = get_pair()
     while get_pair() != "":
-        pair = get_pair()
         print(f"to be appended:{pair}")
         name_to_email[pair[0]] = pair[1]
         print(f"dictionary: {name_to_email}")
+        pair = get_pair()
+
+    print("DONE YAAY")
 
 
 def get_pair():
     email = input("Email: ")
-    while len(email.split("@")) != 2:  # only 1 @ symbol is present in a valid email
-        email = input("INVALID! Re-enter input: ")
-    name = email.split("@")[0]
+    if email == "":
+        return ""
+    name = email.split("@")[0]  # take portion before @ symbol
     prompt = input(f"is your name {name}? (Y/N) ").lower()
-    while prompt not in ['y', 'n']:
-        print("Invalid response.")
-        prompt = input(f"is your name {name}? (Y/N) ").lower()
-    if prompt == 'n':
+    while prompt not in ['y', '']:
         name = input("Name: ")
 
     return [name, email]
