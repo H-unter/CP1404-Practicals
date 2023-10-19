8ba0125c1430661c606b7eb3e222cd19370785dc
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-19 19:52:47 +1000

    Add testing functionality to guitar_test.py

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -8,9 +8,13 @@ from guitar import Guitar
 
 def main():
     guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
-    print(f"{guitar}")
+    another_guitar = Guitar("Another guitar", 2013, 4.50)
 
-    print(guitar.is_vintage())
+    print(f"{guitar.name} get_age() - Expected {2023 - 1922}. Got {guitar.get_age()}")
+    print(f"{another_guitar.name} get_age() - Expected {2023 - 2013}. Got {another_guitar.get_age()}")
+
+    print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected {False}. Got {another_guitar.is_vintage()}")
 
 
 main()
