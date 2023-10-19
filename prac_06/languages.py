edf7d40f65592a58299777442167422241f25251
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-19 19:31:54 +1000

    Implement looping through languages and print those that are dynamically typed

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -13,5 +13,11 @@ def main():
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
     print(python)
 
+    languages = [python, ruby, visual_basic]
+    print(f"The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
 
 main()
