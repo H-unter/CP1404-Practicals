dbfecea33e8c9cd845cec7736752873e7e6c4eca
H-unter <99230734+H-unter@users.noreply.github.com>
2023-10-27 10:01:17 +1000

    Correct the exemption of guitars.py (misinterpretation of task)

---
+++ prac_06/guitars.py
@@ -0,0 +1,29 @@
+"""
+
+Start: 7:35
+End: 8:15
+"""
+
+from guitar import Guitar
+
+
+def main():
+    guitars = []
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = int(input("Cost: "))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost} added.")
+        name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars.append(Guitar("Fender Stratocaster", 2010, 765.40))
+    for i, guitar in enumerate(guitars):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
