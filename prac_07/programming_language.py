b4850b101d1b136e9258208c50b28f7ee1ac00e7
H-unter <99230734+H-unter@users.noreply.github.com>
2023-11-02 11:37:20 +1000

    Add pointer_arithmetic field to Programming_language class

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,16 +7,17 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, pointer_arithmetic, year):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
+        self.pointer_arithmetic = pointer_arithmetic
         self.year = year
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, Pointer Arithmetic={self.pointer_arithmetic}, First appeared in {self.year}"
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
