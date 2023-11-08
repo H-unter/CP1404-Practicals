c629419b0b03a4e16988b658eb710f92a1835467
H-unter <99230734+H-unter@users.noreply.github.com>
2023-11-08 15:41:37 +1000

    Complete box layout exercise

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,5 +8,14 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_clear(self):
+        print("clear!")
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
+
+    def handle_greet(self):
+        print("greetings!")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
 
 BoxLayoutDemo().run()
