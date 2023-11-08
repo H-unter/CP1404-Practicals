c4adb54337994240355ae8ae21e9fe7edccc7f72
H-unter <99230734+H-unter@users.noreply.github.com>
2023-11-08 17:41:34 +1000

    Complete dynamic labels exercise

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
+
+
+class DynamicLabelsApp(App):
+    """Main program - Kivy app to demo dynamic label creation."""
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        self.names = ["Ben", "Liam", "Joel", "David", "John", "Darcy", "Hunter"]
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        """Create labels from data and add them to the GUI."""
+        for name in self.names:
+            new_label = Label(text=name)
+            self.root.ids.main.add_widget(new_label)
+
+
+DynamicLabelsApp().run()
