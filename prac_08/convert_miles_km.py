dd298e408062d6c47d3627e856de5ec3c0dfd53d
H-unter <99230734+H-unter@users.noreply.github.com>
2023-11-08 17:12:21 +1000

    Complete convert_miles_km

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -13,5 +13,20 @@ class ConvertMilesKmApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
+    def handle_conversion(self, value):
+        try:
+            result = str(float(value) * 1.609)
+            self.root.ids.output_label.text = str(result)
+        except:
+            pass
+
+    def increment_input(self, value):
+        try:
+            current_value = float(self.root.ids.input_number.text)
+            new_value = current_value + float(value)
+            self.root.ids.input_number.text = str(new_value)
+        except ValueError:
+            self.root.ids.input_number.text = "Enter a number"
+
 
 ConvertMilesKmApp().run()
--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -14,6 +14,7 @@ __author__ = 'Lindsay Ward'
 
 class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
+
     def build(self):
         """ build the Kivy app from the kv file """
         Window.size = (200, 100)
