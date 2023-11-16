6032cf14a30f82998c94d9a65dfd57e81ddd4d1d
H-unter <99230734+H-unter@users.noreply.github.com>
2023-11-16 14:49:37 +1000

    Complete functionality of band program

--- prac_09/band.py
+++ prac_09/band.py
@@ -10,11 +10,10 @@ class Band():
 
     def __repr__(self):
         musicians_str = ', '.join(str(musician) for musician in self.musicians)
-        return f"{self.name}:\n{musicians_str}"
+        return f"{self.name} {musicians_str}"
 
     def add(self, new_musician):
         self.musicians.append(new_musician)
 
     def play(self):
-        for i in self.musicians:
-            print(f"{self.musicians[i]}")
+        return '\n'.join([musician.play() for musician in self.musicians])
